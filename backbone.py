857c764d23fd6840399f6b4ba4e3904c36e60962
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-01-26 14:38:04 +0800

    Update and rename backbone.py to resnet50.py

--- backbone.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn as nn
-import torchvision.models as models
-
-net = models.resnet34(pretrained=True)
-my_net = nn.Sequential(*list(net.children())[:-1])
-print(my_net)
-
