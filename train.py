0c0f2509849cd892d5f24d4598d2766549ca5e07
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2022-03-06 11:26:47 +0800

    Update train.py

--- train.py
+++ train.py
@@ -4,8 +4,8 @@ import torch.backends.cudnn as cudnn
 import torch.optim as optim
 from torch.utils.data import DataLoader
 
-from nets.frcnn import FasterRCNN
-from nets.frcnn_training import FasterRCNNTrainer, weights_init
+from net.frcnn import FasterRCNN
+from net.frcnn_training import FasterRCNNTrainer, weights_init
 from utils.callbacks import LossHistory
 from utils.dataloader import FRCNNDataset, frcnn_dataset_collate
 from utils.utils import get_classes
